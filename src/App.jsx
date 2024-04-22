f5086ae5eeedacfbd67cc8de06ce2ddf1c6ae5d6
Matheus <desamatheus@gmail.com>
2024-04-22 16:10:15 -0300

    todoas telas prontas

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import Home from './componentes/pages/Home';
 import './styles/main.css';
 import './styles/normalize.css';
 import './styles/fontawesome.min.css';
+import Users from './componentes/pages/Users';
+import UserBlog from './componentes/pages/UserBlog';
+import PostForm from './componentes/pages/PostForm';
 
 function App() {
-  return <Home />;
+  return <PostForm />;
 }
 export default App;
