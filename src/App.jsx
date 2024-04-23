be80cc32bfddb35f61239a2cd2aec61a3b226a71
Matheus <desamatheus@gmail.com>
2024-04-23 12:29:24 -0300

    pronto com root

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,22 @@ import './styles/fontawesome.min.css';
 import Users from './componentes/pages/Users';
 import UserBlog from './componentes/pages/UserBlog';
 import PostForm from './componentes/pages/PostForm';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <PostForm />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/users/:userId' element={<UserBlog />} />
+        <Route path='/users/:userId/post' element={<PostForm />} />
+        <Route path='*' element={<h1>Not found </h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
+
+/* melhorar a rota de notfound,pesquisar no youtube se tem como
+ colocar 404 */
