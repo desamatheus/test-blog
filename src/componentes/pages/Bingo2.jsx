18efee1f7015b3ce5c21ed9203d2f5770644deef
Matheus <desamatheus@gmail.com>
2024-06-25 13:03:12 -0300

    a

--- src/componentes/pages/Bingo2.jsx
+++ src/componentes/pages/Bingo2.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/bingo.css';
 
 function Bingo2() {
-  const [vermelhos, setVermelhos] = useState({});
+  const [vermelhos, setVermelhos] = useState(initializeState());
   const [ultimaDivClicada, setUltimaDivClicada] = useState('');
   const [winnArray, setWinnArray] = useState('');
 
-  let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  let cartela1 = [
+    [1, 15, 36, 50, 65],
+    [12, 24, 60, 74, 90],
+    [2, 11, 22, 32, 69],
+
+    [10, 25, 46, 64, 75],
+    [16, 29, 40, 76, 81],
+    [2, 11, 22, 32, 69],
+
+    [1, 15, 36, 50, 65],
+    [12, 24, 60, 74, 90],
+    [2, 11, 22, 32, 69],
+  ];
   let num2 = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
   let num3 = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45];
   let num4 = [1, 2, 3, 4, 5, 6, 60, 68, 69, 90, 11, 12, 13, 14, 15];
@@ -16,31 +28,34 @@ function Bingo2() {
   let num8 = [66, 77, 88, 19, 20, 21, 22, 43, 34, 25, 16, 27, 28, 29, 30];
   let num9 = [61, 52, 53, 84, 35, 36, 37, 78, 39, 40, 41, 62, 63, 84, 75];
 
-  const initializeState = () => {
+  function initializeState() {
     const initialState = {};
     for (let i = 1; i <= 90; i++) {
       initialState[`div${i}`] = 'op2';
     }
     return initialState;
-  };
+  }
 
   const handleClick = (div) => {
-    setVermelhos((prevState) => ({
-      ...prevState,
-      [div]: prevState[div] === 'op1' ? 'op2' : 'op1',
-    }));
-    setUltimaDivClicada(div);
-    checkWinner();
+    setVermelhos((prevState) => {
+      const newState = {
+        ...prevState,
+        [div]: prevState[div] === 'op1' ? 'op2' : 'op1',
+      };
+      checkWinner(newState, div);
+      return newState;
+    });
   };
 
-  const checkWinner = () => {
+  const checkWinner = (currentVermelhos, lastClickedDiv) => {
     const numerosSorteados = [];
     for (let i = 1; i <= 90; i++) {
-      if (vermelhos[`div${i}`] === 'op1') {
+      if (currentVermelhos[`div${i}`] === 'op1') {
         numerosSorteados.push(i);
       }
     }
-    if (num1.every((numero) => numerosSorteados.includes(numero))) {
+
+    if (cartela1.every((numero) => numerosSorteados.includes(numero))) {
       setWinnArray('Cartela 1');
     } else if (num2.every((numero) => numerosSorteados.includes(numero))) {
       setWinnArray('Cartela 1');
@@ -61,6 +76,7 @@ function Bingo2() {
     } else {
       setWinnArray('');
     }
+    setUltimaDivClicada(lastClickedDiv);
   };
 
   const renderDivs = () => {
@@ -80,7 +96,7 @@ function Bingo2() {
     return divs;
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     setVermelhos(initializeState());
   }, []);
 
